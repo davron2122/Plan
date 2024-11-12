3e7dc8dde8f404972d95fc9a8c8c9ec29ea93f06
davron2122 <155818028+davron2122@users.noreply.github.com>
2024-11-12 23:18:56 +0900

    BRR: building express web server

---
+++ server.js
@@ -0,0 +1,41 @@
+console.log("web serverni boshlash");
+const express = require ("express");
+const app = express();
+
+//gitda nima commit qilganimizni :  git log --online orqali
+
+
+//1.: Kirish code
+app.use (express.static("public"));               // har qanday browserdan public folder ochi, faqat publicni kora oladi
+app.use(express.json());                          // data json formatni objectga ogirib beradi
+app.use(express.urlencoded({extended: true}));    // form inputga yozilgan datani express qabul qilishi uchun, true bolmasa ignore qiladi
+const http =require("http");                      //htttp core module
+
+//2: Sesion
+
+
+//3: Views code
+//traditional usul - BSSR(backend side  server rendering) - Backendda front end ni qurib olish
+//EJS orqali backend ichida htmln yasaladi
+
+app.set("set", "views");
+app.set("view engine", "ejs");
+
+
+//4. Routing code - addresslarni shaklllantirish
+
+app.get("/hello", function(req, res){
+res.end(`<h1 style="background: red">hello world by Daniel</h1>`)
+
+});
+
+app.get("/gift", function(req, res){
+    res.end(`<h1>siz sovgalar bolimidasz</h1>`)
+    
+    });
+
+const server=http.createServer(app);
+let PORT =3000;
+server.listen(PORT, function() {
+    console.log(`The server is running successfully port: ${PORT}`);
+});
