52c72af3d454e307b5050b4673c10641c2cd71a8
davron2122 <155818028+davron2122@users.noreply.github.com>
2024-11-13 20:20:19 +0900

    added purchase.ejs and modified server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 console.log("web serverni boshlash");
 const express = require ("express");
 const app = express();
+const res =require("express/lib/response");
+const http =require("http");  //htttp core module
 
 //gitda nima commit qilganimizni :  git log --online orqali
 
@@ -9,7 +11,7 @@ const app = express();
 app.use (express.static("public"));               // har qanday browserdan public folder ochi, faqat publicni kora oladi
 app.use(express.json());                          // data json formatni objectga ogirib beradi
 app.use(express.urlencoded({extended: true}));    // form inputga yozilgan datani express qabul qilishi uchun, true bolmasa ignore qiladi
-const http =require("http");                      //htttp core module
+                    
 
 //2: Sesion
 
@@ -24,15 +26,25 @@ app.set("view engine", "ejs");
 
 //4. Routing code - addresslarni shaklllantirish
 
-app.get("/hello", function(req, res){
-res.end(`<h1 style="background: red">hello world by Daniel</h1>`)
+app.post("/create-item", (req, res)=>{
+    console.log(req.body);
+    res.json({test: "success"});
+})
 
+app.get("/", function(req, res){
+    res.render("purchase");
+    
 });
 
-app.get("/gift", function(req, res){
-    res.end(`<h1>siz sovgalar bolimidasz</h1>`)
+// app.get("/hello", function(req, res){
+// res.end(`<h1 style="background: red">hello world by Daniel</h1>`)
+
+// });
+
+// app.get("/gift", function(req, res){
+//     res.end(`<h1>siz sovgalar bolimidasz</h1>`)
     
-    });
+//     });
 
 const server=http.createServer(app);
 let PORT =3000;
