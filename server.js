a98ec10013a190ab6cba46f91ed5f32d573869f1
davron2122 <155818028+davron2122@users.noreply.github.com>
2024-11-14 23:45:23 +0900

    feat:develop author router ejs page

--- server.js
+++ server.js
@@ -3,14 +3,24 @@ const express = require ("express");
 const app = express();
 const res =require("express/lib/response");
 const http =require("http");  //htttp core module
+const fs = require("fs");   // file system 
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) =>{
+    if (err){
+        console.log("ERROR:", err);
+    }else {
+        user = JSON.parse(data)
+    }
+});
 
 //gitda nima commit qilganimizni :  git log --online orqali
 
 
 //1.: Kirish code
-app.use (express.static("public"));               // har qanday browserdan public folder ochi, faqat publicni kora oladi
-app.use(express.json());                          // data json formatni objectga ogirib beradi
-app.use(express.urlencoded({extended: true}));    // form inputga yozilgan datani express qabul qilishi uchun, true bolmasa ignore qiladi
+app.use (express.static("public"));               // har qanday browserdan public folder ochi, faqat publicni kora oladi      => Middleware DP(design pattern)
+app.use(express.json());                          // data json formatni objectga ogirib beradi                                => Middleware DD => rest api
+app.use(express.urlencoded({extended: true}));    // form inputga yozilgan datani express qabul qilishi uchun, true bolmasa ignore qiladi =>Middleware DP => Traditional Api 
                     
 
 //2: Sesion
@@ -24,12 +34,18 @@ app.set("set", "views");
 app.set("view engine", "ejs");
 
 
-//4. Routing code - addresslarni shaklllantirish
+//4. Routing code - addresslarni shaklllantirish  - end pointlar hisoblanadi
 
 app.post("/create-item", (req, res)=>{
     console.log(req.body);
     res.json({test: "success"});
-})
+});
+
+app.get("/author", (req, res) =>{
+    res.render("author",{user: user});
+
+});
+
 
 app.get("/", function(req, res){
     res.render("purchase");
@@ -51,3 +67,8 @@ let PORT =3000;
 server.listen(PORT, function() {
     console.log(`The server is running successfully port: ${PORT}`);
 });
+
+
+//APi request : traditional Api, Rest Api, Graphl APi
+// Request Structure:  Header &Body
+// api request methods: GET & POST
