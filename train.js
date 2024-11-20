10c2b299a3a49cb6e3251fc45043ae80bebfeeb3
davron2122 <155818028+davron2122@users.noreply.github.com>
2024-11-20 20:02:07 +0900

    Task-A

--- train.js
+++ train.js
@@ -1,42 +1,86 @@
-console.log("Jack Ma maslaxatlari");
-const list = [
-    "Yaxshi talaba boling", //0-20
-    "Togri boshliq tanlang va koproq xato qiling", //20-30
-    "Ozingiz uchun ishlashni boshlang", //40-50
-    "Siz kuchli bolgan narsalarmi qiling ", //50-60
-    "Yoshlarga investitsya qiling", //50-60
-    "Endi dam oling, foydasi yoq", //60
-];
+// Task-A
+//Shunday 2 parametrli function tuzing, hamda birinchi parametrdagi letterni ikkinchi parametrdagi so'zdan qatnashga sonini return qilishi kerak boladi.
+//Masalan, countLetter ("e", "engineer") 3ni return qilsin.
+//For loop dan foydalaning
 
-//Synchronous functions
-//Callback Function
-function maslaxatBering(a, callback) {
-    if (typeof a !== "number") callback ("insert a number", null);
-
-    else if (a <= 20) callback (null, list[0]);
-    else if (a > 20 && a <= 30) callback (null, list[1]);
-    else if (a > 30 && a <= 40) callback (null, list[2]);
-    else if (a > 40 && a <= 50) callback (null, list[3]);
-    else if (a > 50 && a <= 60) callback (null, list[4]);
-    else {
-            setInterval(() => {                          
-                callback(null, list[5]);
-            }, 1000);
+function countLetter(letter, setLetter) {
+    let count = 0;
+    for (let i = 0; i < setLetter.length; i++) {
+        if (setLetter[i] === letter) {
+            count++;
         }
     }
+    return count;
+}
+const result = countLetter("3", "353");
+console.log("result:", result);
+
+// via Filter and Split method
+/*
+function countLetter(letter, element) {
+  return element.split("").filter((ele) => ele === letter).length;
+}
+console.log(countLetter("e", "engineer"));
 
 
-    console.log("passed here 0 ");
-    maslaxatBering( 65, (err, data) => {
-        if (err) console.log("ERROR:", err);
-        else {
-            console.log("javob:", data);
-        }
-    });
-    console.log("passed here 1");
+*/
+
+//via Foreach method
+/*
+function countLetter(letter, element) {
+  let count = 0;
+  element.split("").forEach((ele) => {
+    if (ele === letter) {
+      count++;
+    }
+  });
+  return count;
+}
+const result = countLetter("e", "employee");
+console.log("result:", result);
+*/
+
+//--------------------------------------------------------------
+
+// console.log("Jack Ma maslaxatlari");
+// const list = [
+//     "Yaxshi talaba boling", //0-20
+//     "Togri boshliq tanlang va koproq xato qiling", //20-30
+//     "Ozingiz uchun ishlashni boshlang", //40-50
+//     "Siz kuchli bolgan narsalarmi qiling ", //50-60
+//     "Yoshlarga investitsya qiling", //50-60
+//     "Endi dam oling, foydasi yoq", //60
+// ];
+
+// //Synchronous functions
+// //Callback Function
+// function maslaxatBering(a, callback) {
+//     if (typeof a !== "number") callback ("insert a number", null);
+
+//     else if (a <= 20) callback (null, list[0]);
+//     else if (a > 20 && a <= 30) callback (null, list[1]);
+//     else if (a > 30 && a <= 40) callback (null, list[2]);
+//     else if (a > 40 && a <= 50) callback (null, list[3]);
+//     else if (a > 50 && a <= 60) callback (null, list[4]);
+//     else {
+//             setInterval(() => {
+//                 callback(null, list[5]);
+//             }, 1000);
+//         }
+//     }
 
 
+//     console.log("passed here 0 ");
+//     maslaxatBering( 65, (err, data) => {
+//         if (err) console.log("ERROR:", err);
+//         else {
+//             console.log("javob:", data);
+//         }
+//     });
+//     console.log("passed here 1");
 
+
+//---------------------------------------------------------------
 
 //Asynchrnous fuction define qismda ishlatilyapti
 
@@ -50,14 +94,13 @@ function maslaxatBering(a, callback) {
 //     else if (a > 30 && a <= 40) return (list[2]);
 //     else if (a > 40 && a <= 50) return (list[3]);
 //     else if (a > 50 && a <= 60) return (list[4]);
-//     else {
+//     else {dev
 //         return new Promise((resolve, reject) => {     // promise - array map uchun uchun ishlatiladi : setTimeout va setIntervalTime kop ishlatildi
 //             setTimeout(() => {                        // resolve = return, reject = throw new Error ga teng bolayapti
 //                 resolve(list[5]);
 //             }, 5000);
 //         });
 //     }
-
 // }
 
 
@@ -73,8 +116,8 @@ function maslaxatBering(a, callback) {
 //     console.log("passed here 1");
 
 
-//Synchronous toliq ishga tushib bolgach, nodejs asynchronous natija bilan shug'ullanadi   
-// Aynchronous single thread band qilmaydi, operatsiyalarni event loop orqali thread poolga tashlaydi 
+//Synchronous toliq ishga tushib bolgach, nodejs asynchronous natija bilan shug'ullanadi
+// Aynchronous single thread band qilmaydi, operatsiyalarni event loop orqali thread poolga tashlaydi
 
 
 // //call qism da then/catch method dan foydalanilyapti
@@ -94,7 +137,7 @@ function maslaxatBering(a, callback) {
 
 
 
-//Call qismda - asyn/await orqali yaratyappiz 
+//Call qismda - asyn/await orqali yaratyappiz
 // async function run() {
 //     let javob = await maslaxatBering(20);
 //     console.log(javob);
