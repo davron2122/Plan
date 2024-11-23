9f025981a57b4f6feb6c33ee905b53a59231e42b
davron2122 <155818028+davron2122@users.noreply.github.com>
2024-11-23 17:16:40 +0900

    CRUD operations

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ console.log("web serverni boshlash");
 
 const express = require("express")
 const res = require("express/lib/response");
-const http = require("http"); 
+const http = require("http");
 const app = express();           //htttp core module
 const fs = require("fs");                // file system - json file ni chaqirish uchun ishlatilyapti
-const db = require("./server").db();      //MongoDB ni chaqirish 
+const db = require("./server").db();      //calling the MongoDB 
 
 
 
@@ -41,19 +41,35 @@ app.set("view engine", "ejs");
 //4. Routing code - addresslarni shaklllantirish  - end pointlar hisoblanadi
 
 app.post("/create-item", (req, res) => {
+    console.log("user entered /create-item");
     console.log(req.body);
-    res.json({ test: "success" });
-});
-
-app.get("/author", (req, res) => {
-    res.render("author", { user: user });
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({ reja: new_reja }, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.end("something went wrong")
+        } else {
+            console.log(data);
+            res.end("successfull added");
+        }
+    });
 
 });
 
 
 app.get("/", function (req, res) {
-    res.render("plans");
-
+    console.log("user entered /");
+    db.collection("plans")
+        .find()
+        .toArray((err, data) => {
+            if (err) {
+                console.log(err);
+                res.end("something went wrong");
+            } else {
+    
+                res.render("plans", {items: data});
+            }
+        });
 });
 
 module.exports = app;
