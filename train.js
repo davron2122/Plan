c022fe7fdf4b56f5bde7e030ee64db856ab60e43
davron2122 <155818028+davron2122@users.noreply.github.com>
2024-11-27 00:08:23 +0900

    modified: TASK-C is  done

--- train.js
+++ train.js
@@ -1,4 +1,63 @@
 /*
+TASK-C.
+
+MITASK-C 
+
+Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
+MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
+*/
+
+class Shop {
+  constructor(non, lagmon, cola) {
+    this.non = non;
+    this.lagmon = lagmon;
+    this.cola = cola;
+  }
+
+  // Qoldiq metodini yaratish
+  qoldiq() {
+    const currentTime = new Date()
+    .toLocaleTimeString('en-GB', { hour12: false });
+    console.log(`Hozir ${currentTime}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`);
+  }
+
+  
+  sotish(mahsulot, miqdor) {
+    if (mahsulot === 'non') {
+      this.non -= miqdor;
+    } else if (mahsulot === 'lagmon') {
+      this.lagmon -= miqdor;
+    } else if (mahsulot === 'cola') {
+      this.cola -= miqdor;
+    }
+    const currentTime = new Date().toLocaleTimeString('en-GB', { hour12: false });
+    console.log(`Sotish amalga oshirildi: ${miqdor}ta ${mahsulot} sotildi, hozir ${currentTime}da.`);
+  }
+
+  // Qabul method
+  qabul(mahsulot, miqdor) {
+    if (mahsulot === 'non') {
+      this.non += miqdor;
+    } else if (mahsulot === 'lagmon') {
+      this.lagmon += miqdor;
+    } else if (mahsulot === 'cola') {
+      this.cola += miqdor;
+    }
+    const currentTime = new Date().toLocaleTimeString('en-GB', { hour12: false });
+    console.log(`Qabul amalga oshirildi: ${miqdor}ta ${mahsulot} qabul qilindi, hozir ${currentTime}da.`);
+  }
+}
+
+// Misolni tekshirish:
+const shop = new Shop(4, 5, 2);
+shop.qoldiq(); // Boshlang'ich qoldiqni ko'rsatadi
+shop.sotish('non', 3); // 3ta non sotiladi
+shop.qabul('cola', 4); // 4ta cola qabul qilinadi
+shop.qoldiq(); // Yangi qoldiqni ko'rsatadi
+
+
+
+/*
 B-TASK: 
 
 Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
@@ -7,20 +66,6 @@ MASALAN countDigits("ad2a54y79wet0sfgb9") 7ni return qiladi.
 
 
 //solution: 
-
-function countDigit(isNumber) {
-  let count = 0;
-
-  for (let i = 0; i < isNumber.length; i++) {
-    if (!isNaN(isNumber[i]) && isNumber[i] !== " ") {
-      count++;
-    }
-  }
-
-  return count;
-}
-
-console.log("RESULT:", countDigit("ad2a54y79wet0sfgb9"));
 
 
 //-------------------------------------------------------------------------------------
