3f55b21080b547fbce4d7967dd2b82d74b57062e
davron2122 <155818028+davron2122@users.noreply.github.com>
2024-11-28 22:58:46 +0900

    Task-D is done

--- train.js
+++ train.js
@@ -1,3 +1,27 @@
+//TASK-D
+/*
+Shunday function tuzing, u 2ta string parametr ega bolsin, hamda agar har ikkala string bir hil harflardan iborat bolsa true aks holda false qaytarsin
+MASALAN checkContent("mitgroup", "gmtiprou") return qiladi true;
+*/
+
+function haveSameLetters(str1, str2) {
+  const sortString = (str) => str.split("").sort().join("");
+  return sortString(str1) === sortString(str2);
+}
+const result = haveSameLetters("mitgroup", "gmtiprou");
+console.log("result:", result);
+
+console.log("******");
+
+const result2 = haveSameLetters("Nodjs", "isFun");
+console.log("result2:", result2);
+ 
+
+
+
+
+
+
 /*
 TASK-C.
 
@@ -7,53 +31,53 @@ Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass
 MASALAN: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud!
 */
 
-class Shop {
-  constructor(non, lagmon, cola) {
-    this.non = non;
-    this.lagmon = lagmon;
-    this.cola = cola;
-  }
+// class Shop {
+//   constructor(non, lagmon, cola) {
+//     this.non = non;
+//     this.lagmon = lagmon;
+//     this.cola = cola;
+//   }
 
-  // Qoldiq metodini yaratish
-  qoldiq() {
-    const currentTime = new Date()
-    .toLocaleTimeString('en-GB', { hour12: false });
-    console.log(`Hozir ${currentTime}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`);
-  }
+//   // Qoldiq metodini yaratish
+//   qoldiq() {
+//     const currentTime = new Date()
+//     .toLocaleTimeString('en-GB', { hour12: false });
+//     console.log(`Hozir ${currentTime}da ${this.non}ta non, ${this.lagmon}ta lagmon va ${this.cola}ta cola mavjud!`);
+//   }
 
   
-  sotish(mahsulot, miqdor) {
-    if (mahsulot === 'non') {
-      this.non -= miqdor;
-    } else if (mahsulot === 'lagmon') {
-      this.lagmon -= miqdor;
-    } else if (mahsulot === 'cola') {
-      this.cola -= miqdor;
-    }
-    const currentTime = new Date().toLocaleTimeString('en-GB', { hour12: false });
-    console.log(`Sotish amalga oshirildi: ${miqdor}ta ${mahsulot} sotildi, hozir ${currentTime}da.`);
-  }
+//   sotish(mahsulot, miqdor) {
+//     if (mahsulot === 'non') {
+//       this.non -= miqdor;
+//     } else if (mahsulot === 'lagmon') {
+//       this.lagmon -= miqdor;
+//     } else if (mahsulot === 'cola') {
+//       this.cola -= miqdor;
+//     }
+//     const currentTime = new Date().toLocaleTimeString('en-GB', { hour12: false });
+//     console.log(`Sotish amalga oshirildi: ${miqdor}ta ${mahsulot} sotildi, hozir ${currentTime}da.`);
+//   }
 
-  // Qabul method
-  qabul(mahsulot, miqdor) {
-    if (mahsulot === 'non') {
-      this.non += miqdor;
-    } else if (mahsulot === 'lagmon') {
-      this.lagmon += miqdor;
-    } else if (mahsulot === 'cola') {
-      this.cola += miqdor;
-    }
-    const currentTime = new Date().toLocaleTimeString('en-GB', { hour12: false });
-    console.log(`Qabul amalga oshirildi: ${miqdor}ta ${mahsulot} qabul qilindi, hozir ${currentTime}da.`);
-  }
-}
+//   // Qabul method
+//   qabul(mahsulot, miqdor) {
+//     if (mahsulot === 'non') {
+//       this.non += miqdor;
+//     } else if (mahsulot === 'lagmon') {
+//       this.lagmon += miqdor;
+//     } else if (mahsulot === 'cola') {
+//       this.cola += miqdor;
+//     }
+//     const currentTime = new Date().toLocaleTimeString('en-GB', { hour12: false });
+//     console.log(`Qabul amalga oshirildi: ${miqdor}ta ${mahsulot} qabul qilindi, hozir ${currentTime}da.`);
+//   }
+// }
 
-// Misolni tekshirish:
-const shop = new Shop(4, 5, 2);
-shop.qoldiq(); // Boshlang'ich qoldiqni ko'rsatadi
-shop.sotish('non', 3); // 3ta non sotiladi
-shop.qabul('cola', 4); // 4ta cola qabul qilinadi
-shop.qoldiq(); // Yangi qoldiqni ko'rsatadi
+// // Misolni tekshirish:
+// const shop = new Shop(4, 5, 2);
+// shop.qoldiq(); // Boshlang'ich qoldiqni ko'rsatadi
+// shop.sotish('non', 3); // 3ta non sotiladi
+// shop.qabul('cola', 4); // 4ta cola qabul qilinadi
+// shop.qoldiq(); // Yangi qoldiqni ko'rsatadi
 
 
 
